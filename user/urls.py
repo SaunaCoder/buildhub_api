4da0da01674c80a5e351e3983312c5bbfb984692
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-11-02 16:35:03 +0200

    Create User registration

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh', TokenRefreshView.as_view(), name='refresh_token')
+]
