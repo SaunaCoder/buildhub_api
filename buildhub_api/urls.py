6d3191234c3066203d240bdd798bedaf787c212d
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-11-05 17:22:46 +0200

    Change url to content application

--- buildhub_api/urls.py
+++ buildhub_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('user.urls')),
-    path('api/content/', include('content.urls')),
+    path('api/', include('content.urls')),
 ]
